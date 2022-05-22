702c92c7d62316348cf0ba5cd6f54d2709e140cf
qminh77 <95585061+qminh77@users.noreply.github.com>
2022-05-22 18:51:05 +0700

    Add files via upload

---
+++ packages.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * packages
+ * 
+ * @package Sngine
+ * @author Zamblek
+ */
+
+// fetch bootloader
+require('bootloader.php');
+
+// check if packages enabled
+if(!$system['packages_enabled']) {
+    _error(404);
+}
+
+// page header
+page_header($system['system_title']." &rsaquo; ".__("Packages"));
+
+try {
+
+	// get packages
+	$packages = $user->get_packages();
+	/* assign variables */
+	$smarty->assign('packages', $packages);
+	$smarty->assign('packages_count', count($packages));
+
+} catch (Exception $e) {
+	_error(__("Error"), $e->getMessage());
+}
+
+// page footer
+page_footer("packages");
+
+?>
